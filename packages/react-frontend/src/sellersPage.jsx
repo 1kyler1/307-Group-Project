b6acf8357332b4e1eb65cf254f9b70f836960018
david <davidh0511@hotmai.com>
2025-10-20 22:13:55 -0700

    skeleton page

---
+++ packages/react-frontend/src/sellersPage.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import ItemForm from './itemForm';
+
+export default function sellersPage() {
+
+    const [listings, setListings] = useState([]);
+
+  function handleFormSubmit(newItem) {
+    // For now, just store locally (MVP)
+    setListings([...listings, newItem]);
+  }
+
+
+    return (
+        <div>
+            <h1>Sellers Page</h1>
+            
+            <ItemForm handleSubmit={handleFormSubmit} />
+
+            <ul>
+                {listings.map((item, index) => (
+                <li key={index}>
+                    {item.item} — {item.description}
+                </li>
+                ))}
+            </ul>
+
+        </div>
+    );
+
+
+
+}
