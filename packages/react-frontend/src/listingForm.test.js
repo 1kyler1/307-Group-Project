14057fe86cd4eb75b333d8eecc1d5cf2ec7e5989
tzhiru <antnachtung@gmail.com>
2025-11-19 22:33:12 -0800

    more basic tests

--- packages/react-frontend/src/listingForm.test.js
+++ packages/react-frontend/src/listingForm.test.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { TextEncoder } from 'text-encoding';
 global.TextEncoder = TextEncoder;
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import { NewItemFormPage } from './CreateListingForum';
 //import CreateAccount from "./createAccount";
@@ -17,18 +17,29 @@ test("renders the empty form correctly", () => {
 	expect(screen.getByLabelText("Description")).toBeInTheDocument();
 	expect(screen.getByText("Submit")).toBeInTheDocument();
 });
-/*
+
 const testListing = { 
 	title: "test title",
 	description: "test desc",
-	location: "test loc"
+	location: "test loc",
 	tags: ["tag 1", "tag 2", "tag 3"]
 	};
 	
-test("accepts name input", () => {
+test("accepts form input", () => {
 	render(<NewItemFormPage />);
-	const input = screen.getByLabelText("Title");
+	let input = screen.getByLabelText("Title");
 	fireEvent.change(input, { target: { value: testListing.title } });
 	expect(input).toHaveValue(testListing.title);
+	
+	input = screen.getByLabelText("Description");
+	fireEvent.change(input, { target: { value: testListing.description } });
+	expect(input).toHaveValue(testListing.description);
+	
+	input = screen.getByLabelText("Location");
+	fireEvent.change(input, { target: { value: testListing.location } });
+	expect(input).toHaveValue(testListing.location);
+	
+	input = screen.getByLabelText("Tags");
+	fireEvent.change(input, { target: { value: testListing.tags } });
+	expect(input).toHaveValue("tag 1,tag 2,tag 3");
 });
-*/
