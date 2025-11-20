644501ae1a0ea301575a580594f2d40d34691eac
tzhiru <antnachtung@gmail.com>
2025-11-20 00:01:13 -0800

    fixing override syntax

--- packages/react-frontend/eslint.config.js
+++ packages/react-frontend/eslint.config.js
@@ -26,6 +26,7 @@ export default defineConfig([
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
     },
 	overrides: [
+		{
 		files: [
 			"testSetUp.js",
 			"setupAfterEnv.js",
@@ -34,6 +35,7 @@ export default defineConfig([
 		rules: {
                 "no-undef": "off"
             }
+		},
 	],
   },
 ]);
