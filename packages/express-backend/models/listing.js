5a817a98f1dbd3bb8d4c1cc695715c1fec0f4f5d
david <davidh0511@hotmai.com>
2025-12-04 10:33:21 -0800

    fix

--- packages/express-backend/models/listing.js
+++ packages/express-backend/models/listing.js
@@ -4,7 +4,7 @@ const ItemSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     imageUrl: { type: String },
-    pice: { type: Number, required: true},
+    price: { type: Number, required: true},
     description: { type: String, required: true },
     location: { type: String, required: true },
     tags: { type: [String], default: [] },
